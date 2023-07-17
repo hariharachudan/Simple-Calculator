610dc608b19e5c4190592fbbcb78fb9c9dc8266e
hariharachudan <116170870+hariharachudan@users.noreply.github.com>
2023-07-17 12:41:16 +0530

    Add files via upload

---
+++ list n.py
@@ -0,0 +1,51 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Error: Cannot divide by zero."
+
+def simple_calculator():
+    print("Welcome to Simple Calculator!")
+
+    while True:
+        print("\nChoose an operation:")
+        print("1. Add")
+        print("2. Subtract")
+        print("3. Multiply")
+        print("4. Divide")
+        print("5. Exit")
+
+        choice = input("Enter your choice (1/2/3/4/5): ")
+
+        if choice == '5':
+            print("Thank you for using Simple Calculator. Goodbye!")
+            break
+
+        num1 = float(input("Enter the first number: "))
+        num2 = float(input("Enter the second number: "))
+
+        if choice == '1':
+            result = add(num1, num2)
+        elif choice == '2':
+            result = subtract(num1, num2)
+        elif choice == '3':
+            result = multiply(num1, num2)
+        elif choice == '4':
+            result = divide(num1, num2)
+        else:
+            print("Invalid input. Please try again.")
+            continue
+
+        print(f"Result: {result}")
+
+if __name__ == "__main__":
+    simple_calculator()
